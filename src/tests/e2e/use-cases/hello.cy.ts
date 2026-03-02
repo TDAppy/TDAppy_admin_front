ea52fe49f9bfa224b8a99246cdda0dd064d5dc45
Kevin8637 <kevin.lar@live.fr>
2026-03-02 17:09:32 +0100

    Add e2e tests

--- src/tests/e2e/use-cases/hello.cy.ts
+++ src/tests/e2e/use-cases/hello.cy.ts
@@ -1,7 +1,7 @@
 // hello.cy.ts
 describe('My First Test', () => {
   it('Visits the Angular app', () => {
-    cy.visit('/'); // localhost:4200/
-    cy.contains('TDAppy');
+    cy.visit('/');
+    cy.contains('Hello');
   });
 });
